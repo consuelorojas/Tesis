9e9f6628d770f56a1c7ee2b5eaf58c812e53304e
consuelorojas <consuelo.rojas@ing.uchile.cl>
2023-10-20 12:55:47 -0300

    creation of class to manage data

---
+++ Librerias/dataset.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import scipy.io
+from scipy.signal import butter, filtfilt, hilbert
+
+import pandas as pd
+import scipy.io
+from scipy.signal import butter, filtfilt, hilbert
+
+class MatFileToDataFrame:
+    '''
+    A class that converts a MATLAB .mat file to a pandas DataFrame with additional signal processing steps.
+
+    Attributes:
+    -----------
+    file_path : str
+        The path to the .mat file to be converted.
+    variable_name : str
+        The name of the variable in the .mat file to be converted.
+    cutoff : float or tuple
+        The cutoff frequency or frequencies for the bandpass filter.
+
+    Methods:
+    --------
+    get_dataframe(cutoff)
+        Returns a pandas DataFrame with the original signal, signal minus mean, filtered signal, and Hilbert transform.
+    butter_bandpass_filter(signal, cutoff, fs=1000, order=1)
+        Applies a Butterworth bandpass filter to the input signal.
+    hilbert_transform(signal)
+        Applies a Hilbert transform to the input signal to obtain the amplitude envelope.
+    '''
+    def __init__(self, file_path, variable_name, cutoff):
+        '''
+        Initializes the MatFileToDataFrame object.
+
+        Parameters:
+        -----------
+        file_path : str
+            The path to the .mat file to be converted.
+        variable_name : str
+            The name of the variable in the .mat file to be converted.
+        cutoff : float or tuple
+            The cutoff frequency or frequencies for the bandpass filter.
+        '''
+        self.file_path = file_path
+        self.variable_name = variable_name
+        self.cutoff = cutoff
+    
+    def get_dataframe(self, cutoff):
+        '''
+        Returns a pandas DataFrame with the original signal, signal minus mean, filtered signal, and Hilbert transform.
+
+        Parameters:
+        -----------
+        cutoff : float or tuple
+            The cutoff frequency or frequencies for the bandpass filter.
+
+        Returns:
+        --------
+        df : pandas DataFrame
+            A DataFrame with the original signal, signal minus mean, filtered signal, and Hilbert transform.
+        '''
+        mat_data = scipy.io.loadmat(self.file_path)
+        signal = pd.DataFrame(mat_data[self.variable_name])
+        signal_mean = signal.mean(axis=0)
+        signal_filtered = self.butter_bandpass_filter(signal, cutoff)
+        signal_hilbert = self.hilbert_transform(signal_filtered)
+        df = pd.concat([signal, signal-signal_mean, signal_filtered, signal_hilbert], axis=1)
+        df.columns = ['Original Signal', 'Signal - Mean', 'Filtered Signal', 'Hilbert Transform']
+        return df
+    
+    def butter_bandpass_filter(self, signal, cutoff, fs=1000, order=1):
+        '''
+        Applies a Butterworth bandpass filter to the input signal.
+
+        Parameters:
+        -----------
+        signal : pandas DataFrame
+            The input signal to be filtered.
+        cutoff : float or tuple
+            The cutoff frequency or frequencies for the bandpass filter.
+        fs : int, optional
+            The sampling frequency of the input signal. Default is 1000 Hz.
+        order : int, optional
+            The order of the Butterworth filter. Default is 1.
+
+        Returns:
+        --------
+        signal_filtered : pandas DataFrame
+            The filtered signal.
+        '''
+        nyq = 0.5 * fs
+        normal_cutoff = self.cutoff / nyq
+    
+        if len(cutoff) > 1:
+            b, a = butter(order, normal_cutoff, btype='band', analog=False)
+        else:
+            b, a = butter(order, normal_cutoff, btype='low', analog=False)
+
+        signal_filtered = filtfilt(b, a, signal, axis=0)
+        return signal_filtered
+    
+    def hilbert_transform(self, signal):
+        '''
+        Applies a Hilbert transform to the input signal to obtain the amplitude envelope.
+
+        Parameters:
+        -----------
+        signal : pandas DataFrame
+            The input signal to be transformed.
+
+        Returns:
+        --------
+        amplitude_envelope : pandas DataFrame
+            The amplitude envelope of the input signal.
+        '''
+        analytic_signal = hilbert(signal, axis=0)
+        analytic_signal = pd.DataFrame(analytic_signal)
+        return analytic_signal
